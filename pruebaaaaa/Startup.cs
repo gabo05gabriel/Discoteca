d2d560bbded396f5c72872ddb56480018a0b99bf
Romina Fernanda Calvetty Riveros <crr2025774@est.univalle.edu>
2024-03-27 03:34:29 -0400

    Agregar archivos de proyecto.

---
+++ pruebaaaaa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pruebaaaaa.Startup))]
+namespace pruebaaaaa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
